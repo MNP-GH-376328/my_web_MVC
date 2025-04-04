54fe2c5b2aed48a201cc14a1adaf37791c821948
MNP-GH-376328 <SOURCECODEREVIEW@manappuram.com>
2025-04-04 10:25:44 +0530

    cmt

---
+++ mvc_project/Controllers/contact.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_project.Controllers
+{
+    public class contact : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvc_project/Models/Expens.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc_project.Models
+{
+    public class Expens
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        [Required]
+        public String? Description  { get; set; }
+    }
+}
